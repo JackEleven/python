e20e7338b0946778249c628945ccb1c5ddb29379
Li, Wei C <wei.c.li@intel.com>
2017-07-28 22:18:07 -0700

    Add a project to use numpy package

---
+++ UseScipy/Array.py
@@ -0,0 +1,12 @@
+import sys
+import numpy as np
+
+def main():
+    print('create an array filled with random values')
+    e = np.random.random((2, 2))
+    print(e)
+    
+    
+if __name__ == "__main__": 
+    main()
+    
